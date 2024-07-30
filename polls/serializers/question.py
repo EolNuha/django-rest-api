b44f95a715b50f30524fdb4bc5b1fbe328e609fb
eolnuha <olinuha08@hotmail.com>
2024-07-30 13:45:25 +0200

    [Update] Question restore api

--- polls/serializers/question.py
+++ polls/serializers/question.py
@@ -3,6 +3,10 @@ from ..models import Question
 
 
 class QuestionSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
     class Meta:
         model = Question
-        fields = "__all__"
+        fields = ['id', 'question_text', 'pub_date', 'author']
+        
+    def get_author(self, obj):
+        return obj.author.username
--- polls/views/question.py
+++ polls/views/question.py
@@ -1,10 +1,46 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 from ..models import Question
-from ..serializers import QuestionSerializer
+from ..serializers import QuestionSerializer, EmptySerializer
 from ..pagination import Pagination
 
 
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
     pagination_class = Pagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['question_text', 'author__username']
+    
+    def get_serializer_class(self):
+        if self.action == 'restore':
+            return EmptySerializer
+        return QuestionSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=False, methods=['post'], url_path="(?P<id>[^/.]+)/restore")
+    def restore(self, request, id=None):
+        try:
+            instance = Question.everything.get(id=id)
+            instance.restore()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Question.DoesNotExist:
+            return Response({'detail': 'Question not found.'}, status=404)
+    
+    @action(detail=False, methods=['get'], url_path='by-author/(?P<username>[^/.]+)')
+    def by_author(self, request, username=None):
+        try:
+            user = User.objects.get(username=username)
+            questions = Question.objects.filter(author=user)
+            page = self.paginate_queryset(questions)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(questions, many=True)
+            return Response(serializer.data)
+        except User.DoesNotExist:
+            return Response({'detail': 'User not found.'}, status=404)
